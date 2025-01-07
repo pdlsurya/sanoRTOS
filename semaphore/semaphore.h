8999d9c5292ba21b24c99fcf405fddd3f2c0bd49
Surya Poudel <poudel.surya2011@gmail.com>
2025-01-07 11:10:25 +0545

    Add LICENSE

--- semaphore/semaphore.h
+++ semaphore/semaphore.h
@@ -1,12 +1,25 @@
-/**
- * @file semaphore.h
- * @author Surya Poudel
- * @brief Semaphore APIs
- * @version 0.1
- * @date 2024-04-09
+/*
+ * MIT License
  *
- * @copyright Copyright (c) 2024 Surya Poudel
+ * Copyright (c) 2024 Surya Poudel
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef __SANO_RTOS_SEMAPHORE_H
@@ -31,8 +44,8 @@ extern "C"
  */
 #define SEMAPHORE_DEFINE(name, initialCount, maxCnt) \
     semaphoreHandleType name = {                     \
-        .waitQueue = {0},                                 \
-        .count = initialCount,                            \
+        .waitQueue = {0},                            \
+        .count = initialCount,                       \
         .maxCount = maxCnt}
 
     typedef struct
